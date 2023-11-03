647669b4b2ee42f87ef0e80b3abeec3797cc1cef
nolliechyTW <nolliechy@gmail.com>
2023-11-03 10:01:21 -0400

    Adjust the index by subtracting 1

--- Leetcode/ArrayAndHashing/347. Top K Frequent Elements/solution.py
+++ Leetcode/ArrayAndHashing/347. Top K Frequent Elements/solution.py
@@ -3,13 +3,13 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
         count = {}
-        freq = [[] for i in range(len(nums)+1)]
+        freq = [[] for i in range(len(nums))]
         
         for i in nums:
             count[i] = count.get(i, 0) + 1
             
         for key, value in count.items():
-            freq[value].append(key)
+            freq[value-1].append(key)
             
         ans = []
         for i in range(len(freq)-1,-1,-1):
