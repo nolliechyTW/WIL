cd7ac4543116ff7dfcfc2ee9be14fad67480da42
HuiYu (Nollie) Chen <nolliechy@gmail.com>
2023-11-21 21:34:41 -0500

    update

--- Leetcode/Stack/853. Car Fleet/solution.py
+++ Leetcode/Stack/853. Car Fleet/solution.py
@@ -1,16 +1,22 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        # Combine position and speed into pairs
-        pairs = sorted(zip(position, speed), reverse=True)
+        # Combine position and speed into pairs and sort by position
+        pairs = sorted(zip(position, speed), key=lambda x: x[0])
 
+        # Stack to store the time it takes for each car to reach the destination
         stack = []
+
+        # Iterate through the sorted pairs
         for p, s in pairs:
+            # Calculate the time it takes for the current car to reach the destination
             time_to_destination = (target - p) / s
-            
+
             # Check if the current car can join the previous fleet
-            while stack and time_to_destination <= stack[-1]:
+            while stack and time_to_destination >= stack[-1]:
                 stack.pop()
-                
+
+            # Add the current car's time to the stack
             stack.append(time_to_destination)
 
+        # The length of the stack represents the number of car fleets
         return len(stack)
