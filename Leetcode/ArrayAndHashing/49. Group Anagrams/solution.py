49ca996b906f859965acdeba2bf1aa6ef9142543
HuiYu (Nollie) Chen <nolliechy@gmail.com>
2024-01-25 23:25:51 -0500

    jan 25

--- Leetcode/ArrayAndHashing/49. Group Anagrams/solution.py
+++ Leetcode/ArrayAndHashing/49. Group Anagrams/solution.py
@@ -10,3 +10,15 @@ class Solution:
                 group_by_char[key] = [value]
         # retrieves all the values from the map and turn them into a list of lists
         return list(group_by_char.values())
+    
+# alternate solution
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        group_by_char = defaultdict(list)
+
+        for value in strs:
+            key = ''.join(sorted(value))  # Sort the string and join back into a string
+            group_by_char[key].append(value)
+
+        return list(group_by_char.values())
+    
